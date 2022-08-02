b46754bd7b6049bfbefcc5aa1ae4ddf3293fd799
Salman0112358 <Salman0112358@outlook.com>
2022-08-02 23:13:01 +0100

    removed broken API code used for testing

--- src/API.js
+++ src/API.js
@@ -1,11 +1,1 @@
 ///
-
-import { useEffect } from "react";
-
-export const handleGetEpisodes = async (setCurrentShow) => {
-  const response = await fetch("https://api.tvmaze.com/shows/82/episodes");
-
-  const jsonBody = await response.json();
-
-  return setCurrentShow(jsonBody);
-};
