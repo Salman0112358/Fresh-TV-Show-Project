db390328f833f4dbc09453f9dc2f2b19d358c90a
Salman0112358 <Salman0112358@outlook.com>
2022-08-04 11:51:18 +0100

    saving again

--- src/components/DropDownEpisodeMenu/DropDownEpisodeMenu.tsx
+++ src/components/DropDownEpisodeMenu/DropDownEpisodeMenu.tsx
@@ -12,12 +12,6 @@ export const DropDownEpisodeMenu = ({
   return (
     <Wrapper>
       <MenuDrop>
-        <div className="dropdown">
-          <button className="dropbtn">Show Episodes List</button>
-          <div className="dropdown-content">
-            <ul>{children}</ul>
-          </div>
-        </div>
       </MenuDrop>
     </Wrapper>
   );
